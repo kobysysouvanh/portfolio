ef2804197869345c53c1e535540d79cdd139de33
kobysysouvanh <kobysysouvanh@gmail.com>
2021-12-24 15:26:20 -0500

    replaced message

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,7 +7,7 @@ function Resume() {
     return (
         <div className="flex items-center justify-center flex-col py-10" id="resume"> 
             <h1 className="text-4xl font-bold py-10">Resume</h1>
-            <img src={resume} alt="Resume" className="shadow-xl border-solid border-2 rounded-lg border-gray-100"/>
+            <img src={resume} alt="Resume" className="rounded-lg"/>
         </div>
     )
 }
