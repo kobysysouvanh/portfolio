648536843b85e362d650bb24853023d7a596cdcb
kobysysouvanh <kobysysouvanh@gmail.com>
2022-08-08 15:46:17 -0400

    Added Bill Splitter Project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ function Projects() {
         <div className="flex items-center justify-center flex-col py-10" id="projects">
             <h1 className="text-4xl font-bold py-10">Projects</h1>
             <div>
-                to be implemented...
+                <a className="text-2xl hover:text-sky-300" target="_blank" rel="noreferrer" href="https://bill-splitter-3e4ab.web.app/">Bill Splitter</a>
             </div>
         </div>
     )
