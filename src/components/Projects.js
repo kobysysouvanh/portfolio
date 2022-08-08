bdd9165213436090885649e384fc24f6f3bd03b0
kobysysouvanh <kobysysouvanh@gmail.com>
2022-08-08 16:12:44 -0400

    Portfolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,8 +4,9 @@ function Projects() {
     return (
         <div className="flex items-center justify-center flex-col py-10" id="projects">
             <h1 className="text-4xl font-bold py-10">Projects</h1>
-            <div>
+            <div className="flex items-center justify-center flex-col">
                 <a className="text-2xl hover:text-sky-300" target="_blank" rel="noreferrer" href="https://bill-splitter-3e4ab.web.app/">Bill Splitter</a>
+                <a className="text-2xl hover:text-sky-300" target="_blank" rel="noreferrer" href="https://covid-19-live-tracker-8a348.web.app/">Covid-19 Tracker</a>
             </div>
         </div>
     )
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,7 +7,7 @@ function Resume() {
     return (
         <div className="flex items-center justify-center flex-col py-10" id="resume"> 
             <h1 className="text-4xl font-bold py-10">Resume</h1>
-            <img src={resume} alt="Resume" className="rounded-lg"/>
+            <img src={resume} alt="Resume" className="rounded-lg w-2/5"/>
         </div>
     )
 }
